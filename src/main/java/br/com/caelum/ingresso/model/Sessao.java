298b6a4448a123fd0b27ca49019134df51fd82ed
Aryene Campos <aryene.campos@gmail.com>
2021-06-10 20:02:39 -0300

    atualizado

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,59 +2,70 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
-	public Sessao(Integer id, LocalTime horario, Sala sala, Filme filme) {
-		super();
-		this.id = id;
-		this.horario = horario;
-		this.sala = sala;
-		this.filme = filme;
-	}
+  
+  @Id
+  @GeneratedValue (strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private LocalTime horario;
+  
+  @ManyToOne 
+  private Sala sala;
+  
+  @ManyToOne 
+  private Filme filme;
+  
+  //construtror vazio apenas para uso do hibernate
+  public Sessao () {
+    
+  }
 
-	private Integer id;
-	private LocalTime horario;
-	private Sala sala;
-	private Filme filme;
+  public Sessao(LocalTime horario, Filme filme, Sala sala) {
+    this.horario = horario;
+    this.filme = filme;
+    this.sala = sala;
+  }
 
-	public Sessao(LocalTime horario, Filme filme, Sala sala) {
-		this.horario = horario;
-		this.filme = filme;
-		this.sala = sala;
-	}
-	// criar getters e setters
+  public Integer getId() {
+    return id;
+  }
 
-	public Integer getId() {
-		return id;
-	}
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+  public LocalTime getHorario() {
+    return horario;
+  }
 
-	public LocalTime getHorario() {
-		return horario;
-	}
+  public void setHorario(LocalTime horario) {
+    this.horario = horario;
+  }
 
-	public void setHorario(LocalTime horario) {
-		this.horario = horario;
-	}
+  public Sala getSala() {
+    return sala;
+  }
 
-	public Sala getSala() {
-		return sala;
-	}
+  public void setSala(Sala sala) {
+    this.sala = sala;
+  }
 
-	public void setSala(Sala sala) {
-		this.sala = sala;
-	}
+  public Filme getFilme() {
+    return filme;
+  }
 
-	public Filme getFilme() {
-		return filme;
-	}
+  public void setFilme(Filme filme) {
+    this.filme = filme;
+  }
+  
+  // criar getters e setters
 
-	public void setFilme(Filme filme) {
-		this.filme = filme;
-	}
-	
-	
-	
+ 
 }
