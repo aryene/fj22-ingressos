35484c5fcf74f9843b7dfae6e1a9875886b35fae
Ariane <Ariane@DESKTOP-1QPLJHR>
2021-06-14 23:11:13 -0300

    Criação	o	catálogo	de	filmes	e	tela	de	detalhes	do	filme Criação	descontos	e	ingresso

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -10,62 +12,72 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Sessao {
-  
-  @Id
-  @GeneratedValue (strategy = GenerationType.IDENTITY)
-  private Integer id;
-  private LocalTime horario;
-  
-  @ManyToOne 
-  private Sala sala;
-  
-  @ManyToOne 
-  private Filme filme;
-  
-  //construtror vazio apenas para uso do hibernate
-  public Sessao () {
-    
-  }
 
-  public Sessao(LocalTime horario, Filme filme, Sala sala) {
-    this.horario = horario;
-    this.filme = filme;
-    this.sala = sala;
-  }
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private LocalTime horario;
 
-  public Integer getId() {
-    return id;
-  }
+	@ManyToOne
+	private Sala sala;
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+	@ManyToOne
+	private Filme filme;
 
-  public LocalTime getHorario() {
-    return horario;
-  }
+	private BigDecimal preco;
 
-  public void setHorario(LocalTime horario) {
-    this.horario = horario;
-  }
+	// construtror vazio apenas para uso do hibernate
+	public Sessao() {
 
-  public Sala getSala() {
-    return sala;
-  }
+	}
 
-  public void setSala(Sala sala) {
-    this.sala = sala;
-  }
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
+	}
 
-  public Filme getFilme() {
-    return filme;
-  }
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
 
-  public void setFilme(Filme filme) {
-    this.filme = filme;
-  }
-  
-  // criar getters e setters
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
- 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+	// criar getters e setters
+
 }
